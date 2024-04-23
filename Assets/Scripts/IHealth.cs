795503782b0e636668fb925a05e8853597037122
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-04-22 22:44:47 -0500

    Added IHealth interface for health handling

--- Assets/Scripts/IHealth.cs
+++ Assets/Scripts/IHealth.cs
@@ -2,4 +2,9 @@ using UnityEngine;
 
 public interface IHealth
 {
+    float health { get; set; }
+    float maxHealth { get; set; }
+
+    public float getHealth();
+    public void setHealth(float hp);
 }
